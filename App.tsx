e6f584bd2819fd5310f4fc362abf7103bf9ed176
JakubWiecek <jakub.wiecek.skrzynka@gmail.com>
2024-11-07 22:38:37 +0100

    Lab 5 - Splash screen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 
 import Calculator from './Calculator/Calculator';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+    useEffect(() => {
+        SplashScreen.hide();
+    });
     return (
         <SafeAreaView style={styles.container}>
             <Calculator/>
