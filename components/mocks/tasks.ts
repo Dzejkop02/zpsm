65d277d3846232af2b862a6f8ebb598856cba8d5
JakubWiecek <jakub.wiecek.skrzynka@gmail.com>
2024-12-05 23:55:53 +0100

    Added mock tasks list

---
+++ components/mocks/tasks.ts
@@ -0,0 +1,113 @@
+export const tasks = [
+  {
+    question:
+      'Który wódz po śmierci Gajusza Mariusza prowadził wojnę domową z Sulą?',
+    answers: [
+      {
+        content: 'LUCJISZ CYNNA',
+        isCorrect: true,
+      },
+      {
+        content: 'JULIUSZ CEZAR',
+        isCorrect: false,
+      },
+      {
+        content: 'LUCJUSZ MURENA',
+        isCorrect: false,
+      },
+      {
+        content: 'MAREK KRASSUS',
+        isCorrect: false,
+      },
+    ],
+    duration: 30,
+  },
+  {
+    question: 'Task 2?',
+    answers: [
+      {
+        content: 'A',
+        isCorrect: false,
+      },
+      {
+        content: 'B true',
+        isCorrect: true,
+      },
+      {
+        content: 'C',
+        isCorrect: false,
+      },
+      {
+        content: 'D',
+        isCorrect: false,
+      },
+    ],
+    duration: 30,
+  },
+  {
+    question: 'Task 3?',
+    answers: [
+      {
+        content: 'A',
+        isCorrect: false,
+      },
+      {
+        content: 'B true',
+        isCorrect: true,
+      },
+      {
+        content: 'C',
+        isCorrect: false,
+      },
+      {
+        content: 'D',
+        isCorrect: false,
+      },
+    ],
+    duration: 20,
+  },
+  {
+    question: 'Task 4?',
+    answers: [
+      {
+        content: 'A',
+        isCorrect: false,
+      },
+      {
+        content: 'B',
+        isCorrect: false,
+      },
+      {
+        content: 'C true',
+        isCorrect: true,
+      },
+      {
+        content: 'D',
+        isCorrect: false,
+      },
+    ],
+    duration: 30,
+  },
+  {
+    question: 'Task 5?',
+    answers: [
+      {
+        content: 'A',
+        isCorrect: false,
+      },
+      {
+        content: 'B',
+        isCorrect: false,
+      },
+      {
+        content: 'C',
+        isCorrect: false,
+      },
+      {
+        content: 'D true',
+        isCorrect: true,
+      },
+    ],
+    duration: 10,
+  },
+];
